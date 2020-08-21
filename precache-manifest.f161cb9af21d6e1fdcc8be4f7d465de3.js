eca57b4770c831ee3d4639336d158af516630c5a
Shira Abramovich <shira_abramovich@brown.edu>
2020-08-21 18:55:28 -0400

    Updates

---
+++ precache-manifest.f161cb9af21d6e1fdcc8be4f7d465de3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a65dafa61e0b26228b4e35893a234b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec2f2c686f3f3441d98",
+    "url": "/static/css/main.9845799e.chunk.css"
+  },
+  {
+    "revision": "2c5a69bf0b7def7f5e76",
+    "url": "/static/js/2.8848d14f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8848d14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec2f2c686f3f3441d98",
+    "url": "/static/js/main.0517c56a.chunk.js"
+  },
+  {
+    "revision": "1d2dce6e2bf9d3a4e089",
+    "url": "/static/js/runtime-main.5de58a0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
