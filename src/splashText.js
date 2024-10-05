69dac4878ae2d6603d90071feb594fe1ecf782bd
Shira Abramovich <shira.abramovich@gmail.com>
2024-10-05 19:51:02 -0400

    feat: add media

---
+++ src/splashText.js
@@ -0,0 +1,10 @@
+const splashText = {
+    "en": {
+        "adrienne_audio": "Listen to a recording from Lénaïg Cariou & Camille Blanc's recent workshop on translating gender (FR)",
+    },
+    "fr": {
+        "adrienne_audio": "Traduire en féministe: exergues des traductions sorties d'un atelier mené par Lénaïg Cariou et Camille Blanc (FR)",
+    },
+};
+
+export default splashText;
