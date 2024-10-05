daba91b6989968567c36ce93937bcbfeac356cad
Shira Abramovich <shira.abramovich@gmail.com>
2024-10-05 17:54:10 -0400

    syntax: fix improper props

--- src/Splash.jsx
+++ src/Splash.jsx
@@ -10,9 +10,9 @@ const Splash = ({ lang }) => {
           title="Lecture avec Mónica de la Torre"
           width="560" height="315"
           src="https://www.youtube.com/embed/2lZZnmC3iR4?si=K2ysOMTZ5RR9SFOZ"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
           style={{"margin": "auto"}}
         />
       </span>
